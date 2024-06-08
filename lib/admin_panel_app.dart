d700576821772d9e1b3f9588a69899ce6aefef50
ryokucha <danyh852@gmail.com>
2024-06-08 16:14:20 +0300

    Creating a Project structure

---
+++ lib/admin_panel_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class AdminPanelApp extends StatelessWidget {
+  const AdminPanelApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Placeholder());
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:fittin_admin_panel/admin_panel_app.dart';
+import 'package:flutter/widgets.dart';
+
+void main() {
+  runApp(const AdminPanelApp());
+}
