f4bb905dd1f398d6533b83cf7081d92b0c50e94f
Владимир <astoriya2004@gmail.com>
2024-06-21 17:30:43 +0300

    add class Wrapper and add BlocProvider in this

--- lib/admin_panel_app.dart
+++ lib/admin_panel_app.dart
@@ -1,8 +1,23 @@
 import 'package:fittin_admin_panel/core/navigation/app_router.dart';
 import 'package:fittin_admin_panel/core/style/theme/light_theme.dart';
-import 'package:fittin_admin_panel/features/home/presentation/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fittin_admin_panel/export_all_bloc.dart';
+
+
+class Wrapper extends StatelessWidget{
+  const Wrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(providers: [
+      BlocProvider(create: (context) => HomeBloc()),
+      BlocProvider(create: (context) => CategoryBloc()),
+    ], child: const AdminPanelApp());
+  }
+}
+
 
 class AdminPanelApp extends StatefulWidget {
   const AdminPanelApp({super.key});
